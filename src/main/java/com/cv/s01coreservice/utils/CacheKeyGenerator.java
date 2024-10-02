e71e54d8b8aeb3d55cb12398d25e81d40e6a8761
ramakrishna <ram_r@live.com>
2024-10-02 19:15:41 +0400

    Commit - Core Service - 02-10-2024

--- src/main/java/com/cv/s01coreservice/utils/CacheKeyGenerator.java
+++ src/main/java/com/cv/s01coreservice/utils/CacheKeyGenerator.java
@@ -1,10 +1,12 @@
 package com.cv.s01coreservice.utils;
 
 import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Method;
 
+@Component
 public class CacheKeyGenerator implements KeyGenerator {
     @Override
     public Object generate(Object target, Method method, Object... params) {
